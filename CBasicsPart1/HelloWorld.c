a54e96d4e78d491c90401169deeee33aa534562f
JayClack <jay.clack1998@outlook.com>
2020-03-16 14:30:51 +0000

    A really basic program, created so I can learn how to compile from the terminal

---
+++ CBasicsPart1/HelloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+This code will be compiled via cml, using the compiled installed alongside Visual Studio
+*/
+
+int main()
+{
+	printf("Hello there. This is a C program compiled on the command line.");
+
+	return 0;
+}
