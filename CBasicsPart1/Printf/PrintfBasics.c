b7730f9ff1da79b59a34768289ac46bbaa244cfe
JayClack <jay.clack1998@outlook.com>
2020-03-18 12:07:14 +0000

    Printf pre-work commit

--- CBasicsPart1/Printf/PrintfBasics.c
+++ CBasicsPart1/Printf/PrintfBasics.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /*
-Printf is the basic "output to the standard peripheral" function within C.
+Printf is the basic "output to the standard peripheral" function within C., typically sending messages to the screen.
+
 */
 
 main() {
